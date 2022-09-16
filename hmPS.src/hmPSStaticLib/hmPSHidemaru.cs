c80a412bb64fc19efb4c54d4b31506d9418782b9
komiyamma <komiyama0629@gmail.com>
2022-09-16 19:36:21 +0900

    一応下もフォロー

--- hmPS.src/hmPSStaticLib/hmPSHidemaru.cs
+++ hmPS.src/hmPSStaticLib/hmPSHidemaru.cs
@@ -64,6 +64,17 @@ public sealed partial class hmPSDynamicLib
                     break;
                 }
             }
+            while (true)
+            {
+                if (number < Int32.MinValue)
+                {
+                    number = number + 4294967296;
+                }
+                else
+                {
+                    break;
+                }
+            }
 
             bool success = false;
             if (Int32.MinValue <= number && number <= Int32.MaxValue)
